5e3f7cc845ba937a1c10b0bf86dc63786cde1dd9
antonio <ammj24@hotmail.com>
2019-10-26 15:08:05 +0100

    add title svg and dataset

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 
+d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
+    .then((result) => {
+        heatMap(result);
+    });
+
 d3.select('body')
     .append('h1')
     .attr('id', 'title')
     .text('Monthly Global Land-Surface Temperature')
+
+d3.select('body')
+    .append('h3')
+    .attr('id', 'description')
+    .text('Based on a temperature of 8.66')
+
+function heatMap(dataset) {
+
+    const width = 1000;
+    const height = 500;
+    const padding = 100;
+    
+    d3.select('body')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+
+
+}
+
+
+
+
+
+
