175fc11f44cf720448d9542d487e872c77907b18
antonio <ammj24@hotmail.com>
2019-11-02 05:36:04 +0000

    make axis visible

--- main.js
+++ main.js
@@ -16,28 +16,26 @@ d3.select('body')
 
 function heatMap(dataset) {
 
-    // const width = 1000;
-    // const height = 500;
     const width = 1200;
     const height = 530;
-    const padding = 100;
+    const padding = 80;
 
     const bTemperature = dataset['baseTemperature'];
     const mVariance = dataset['monthlyVariance'];
 
     const months = {
-        0: 'January',
-        1: 'February',
-        2: 'March',
-        3: 'April',
-        4: 'May',
-        5: 'June',
-        6: 'July',
-        7: 'August',
-        8: 'September',
-        9: 'October',
-       10: 'November',
-       11: 'December'
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+       10: 'October',
+       11: 'November',
+       12: 'December'
     };
     
     const svg = d3.select('body')
@@ -46,11 +44,13 @@ function heatMap(dataset) {
                     .attr('height', height);
 
     const xScale = d3.scaleLinear()
-                        .domain([ d3.min(mVariance, (d) => d['year']), d3.max(mVariance, (d) => d['year']) + 2 ])
+                        .domain([d3.min(mVariance, (d) => d['year']), 
+                                 d3.max(mVariance, (d) => d['year']) + 2])
                         .range([padding, width - padding]);
 
     const yScale = d3.scaleLinear()
-                        .domain([ d3.max(mVariance, (d) => d['month']-1), d3.min(mVariance, (d) => d['month'] - 1) ])
+                        .domain([d3.max(mVariance, (d) => d['month']), 
+                                 d3.min(mVariance, (d) => d['month']) - 1])
                         .range([height - padding, padding]);
 
     const xAxis = d3.axisBottom(xScale)
@@ -61,7 +61,6 @@ function heatMap(dataset) {
                     });
 
     const yAxis = d3.axisLeft(yScale)
-                    .ticks(12)
                     .tickSizeOuter(0)
                     .tickFormat((d) => {
                         return months[d];
@@ -79,11 +78,17 @@ function heatMap(dataset) {
         .attr('transform', 'translate(' + padding + ', 0)')
         .call(yAxis)
         .call(g => g.selectAll('.tick line')
-                    .attr('transform', 'translate(0, -14)'))
+                    .attr('transform', 'translate(0, -18)'))
         .call(g => g.selectAll('.tick text')
                     .style('font-size', '0.75rem')
                     .attr('x', '-14')
-                    .attr('dy', '-11'));
+                    .attr('dy', '-15'));
+
+    d3.selectAll('g.tick')
+      .filter(function(d) {
+          console.log(d)
+          return (d === 0)
+      }).remove()
 
     
     let tooltip = d3.select('body')
@@ -93,10 +98,9 @@ function heatMap(dataset) {
                     .style('z-index', '10')
                     .style('opacity', '0.85')
                     .style('border-radius', '5px')
-                    .style('background-color', '#fff')
+                    .style('background-color', 'white')
                     .style('padding', '0.6rem')
                     .style('visibility', 'hidden')
-                    // .style('display', 'flex')
 
     svg.selectAll('rect')
         .data(mVariance)
@@ -117,14 +121,20 @@ function heatMap(dataset) {
                 case (temp < 7):
                         color = 'lightGreen';
                         break;
-                case (temp < 8):
+                case (temp < 7.5):
                         color = 'green';
                         break;
+                case (temp < 8):
+                        color = 'yellow';
+                        break;
+                case (temp < 8.5):
+                        color = 'orange';
+                        break;
                 case (temp < 9):
-                        color = 'peru';
+                        color = 'darkorange';
                         break;
                 case (temp < 10):
-                        color = 'darkorange';
+                        color = 'chocolate';
                         break;
                 default: 
                     color = 'brown';
@@ -134,12 +144,10 @@ function heatMap(dataset) {
         .attr('data-year', (d) => d['year'])
         .attr('data-month', (d) => d['month'] - 1)
         .attr('data-temp', (d) => bTemperature + d['variance'])
-        // .attr('width', '0.3rem')
-        // .attr('height', '1.85rem')
         .attr('width', '0.32rem')
-        .attr('height', '2.1rem')
-        .attr('x', (d) => xScale(d['year']))
-        .attr('y', (d) => yScale(d['month']-2.08))
+        .attr('height', '1.93rem')
+        .attr('x', (d) => xScale(d['year'] + 0.2))
+        .attr('y', (d) => yScale(d['month'] - 1))
         .on('mouseover', (d) => {
             tooltip.style('visibility', 'visible')
             tooltip.attr('data-year', d['year'])
@@ -153,32 +161,31 @@ function heatMap(dataset) {
         .on('mouseout', (d) => tooltip.style('visibility', 'hidden'))
 
 
-        let colors = ['blue', 'green', 'orange', 'brown'];
+        let colors = [['cadetBlue', '3.5'], ['blue', '4'], ['green', '4.5'], ['orange', '5'], 
+                      ['brown', '5.5']];
 
-        let label = svg.append('g')
+        let legend = svg.append('g')
             .attr('id', 'legend')
+        
+        legend.selectAll('rect')
+            .data(colors)
+            .enter()
+            .append('rect')
+            .attr('x', (d, i) => 200 + i * 42)
+            .attr('y', 486)
+            .attr('width', '2.5rem')
+            .attr('height', '1rem')
+            .attr('fill', d => d[0])
 
-        label.selectAll('text')
+        legend.selectAll('text')
             .data(colors)
             .enter()
             .append('text')
             .attr('id', 'legend')
-            .attr('x', ( d, i) => 170 + i * 40 )
-            .attr('y', 494)
-            .text((d) => d)
-        
-        label.selectAll('rect')
-            .data(colors)
-            .enter()
-            .append('rect')
-            .attr('x', (d, i) => 200 + i * 20)
-            .attr('y', 460)
-            .attr('width', '1rem')
-            .attr('height', '1rem')
-            .attr('fill', (d) => d)
-
-     
-            
+            .attr('x', ( d, i) => 200 + i * 46 )
+            .attr('y', 516)
+            .style('font-size', '0.8rem')
+            .text(d => d[1])
 
     
 }
